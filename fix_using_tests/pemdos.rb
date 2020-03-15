493d8431c8a4110d49bd4699ddb7f43fd581e1b5
Enrique Pittaluga <ep7m@me.com>
2020-03-15 15:00:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,14 +1,11 @@
 
 def snake_it_up(string)
-  if string[0] == "s"
-    
-    10.times do
-      string = "s" + string 
-    end
-    
-  else
-  string
-
+    if string[0] == "s"
+      10.times do
+        string = "s" + string 
+      end
+    else
+    string
   end
   string
 end
